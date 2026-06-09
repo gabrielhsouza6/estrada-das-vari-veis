e7a71822af73a70cb07e37a9625ca4a8096b4180
gabrielhsouza6 <gabrielhrosouza6@gmail.com>
2026-06-09 17:11:02 -0300

    Implementações adicionais
    Criação de componente de fase - label_sign
    Desenvolvimento da fase 1

---
+++ scripts/label_sign.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+@export var text: String
+@export var flip_horizontal: bool
+
+func _ready() -> void:
+	$Label.text = text
+	if flip_horizontal:
+		$Sprite2D.flip_h = true
+
+func _on_body_entered(body: Node2D) -> void:
+	$Label.visible = true
+
+
+func _on_body_exited(body: Node2D) -> void:
+	$Label.visible = false
